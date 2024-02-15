95186c726b5c22bf4b49f6c1e1d0602fe70eae7f
GGTuran <tayyabturan67@gmail.com>
2024-02-15 08:57:03 +0600

    not maped

--- src/component/Map.jsx
+++ src/component/Map.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+
 
 const Map = () => {
     return (
         <div>
-            <h2>maped</h2>
-            <p>why did you do this</p>
-            <p>d</p>
+            
+            
+            
         </div>
     );
 };
